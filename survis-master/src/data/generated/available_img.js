0841e149124d407732e56cb74393d8e523b7e276
Avakinds <115653088+Avakinds@users.noreply.github.com>
2024-05-17 07:57:49 +0100

    Delete survis-master directory

--- survis-master/src/data/generated/available_img.js
+++
@@ -1,1 +0,0 @@
-const availableImg = ["Aguilera2015OliveClassification","Bird2022AffectiveComputing","Dandil2019DistanceMeasurement","Ferreyra2018VisualImpairment","Shaqwi2021DeepLearning","Tao2012SeafoodQuality","Yang2021FruitPackaging"];
